42bf6314ff3105de84b10d69c57c959fb09ccf5a
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-01-19 14:18:42 -0500

    Update optional_test.cpp

--- core/tests/optional_test.cpp
+++ core/tests/optional_test.cpp
@@ -7,3 +7,9 @@
 TEST(OptionalTests, constexpr_stuff) {
     // ...
 }
+
+template<typename T> constexpr bool is_trivial = std::is_trivially_destructible<T>::value;
+TEST(OptionalTests, TypeTraits) {
+    static_assert(is_trivial<int>);
+    static_assert(is_trivial<Optional<int>>);
+}
