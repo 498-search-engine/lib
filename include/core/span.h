47e2e4aaef027a9a28bd934ee7149d7386b8be8b
Daniel Sage <dsage@umich.edu>
2025-01-21 22:05:30 -0500

    add tests for span

--- include/core/span.h
+++ include/core/span.h
@@ -8,19 +8,22 @@ namespace core {
 template<typename T>
 class Span {
 public:
+    using iterator = T*;
+    using const_iterator = const T*;
+
     Span(T* ptr, size_t size) : ptr_(ptr), size_(size) {}
 
-    T* Data() const { return ptr_; }
-    size_t Size() const { return size_; }
+    T* data() const { return ptr_; }
+    size_t size() const { return size_; }
 
-    T* begin() const { return ptr_; }
-    T* end() const { return ptr_ + size_; }
-    const T* cbegin() const { return begin(); }
-    const T* cend() const { return end(); }
+    iterator begin() const { return ptr_; }
+    iterator end() const { return ptr_ + size_; }
+    const_iterator cbegin() const { return begin(); }
+    const_iterator cend() const { return end(); }
 
     T& operator[](size_t n) const { return *(ptr_ + n); }
 
-    [[nodiscard]] Span Subspan(size_t offset, size_t size) { return Span{ptr_ + offset, size}; }
+    [[nodiscard]] Span subspan(size_t offset, size_t size) const { return Span{ptr_ + offset, size}; }
 
 private:
     T* ptr_;
---
+++ tests/span_test.cpp
@@ -0,0 +1,59 @@
+#include "core/span.h"
+
+#include <gtest/gtest.h>
+
+using namespace core;
+
+TEST(Span, Initialize) {
+    const char text[] = "hello world";
+    auto a = Span<const char>{text, 5};
+    ASSERT_EQ(a.size(), 5);
+    ASSERT_EQ(a.data(), text);
+    ASSERT_EQ(a[0], 'h');
+    ASSERT_EQ(a[1], 'e');
+    ASSERT_EQ(a[2], 'l');
+    ASSERT_EQ(a[3], 'l');
+    ASSERT_EQ(a[4], 'o');
+}
+
+TEST(Span, Subspan) {
+    const char text[] = "hello world";
+    auto a = Span<const char>{text, sizeof(text) - 1};
+
+    auto b = a.subspan(0, 5);
+    ASSERT_EQ(b.size(), 5);
+    ASSERT_EQ(b[0], 'h');
+    ASSERT_EQ(b[1], 'e');
+    ASSERT_EQ(b[2], 'l');
+    ASSERT_EQ(b[3], 'l');
+    ASSERT_EQ(b[4], 'o');
+
+    auto c = a.subspan(6, 5);
+    ASSERT_EQ(c.size(), 5);
+    ASSERT_EQ(c[0], 'w');
+    ASSERT_EQ(c[1], 'o');
+    ASSERT_EQ(c[2], 'r');
+    ASSERT_EQ(c[3], 'l');
+    ASSERT_EQ(c[4], 'd');
+
+    auto d = c.subspan(1, 2);
+    ASSERT_EQ(d.size(), 2);
+    ASSERT_EQ(d[0], 'o');
+    ASSERT_EQ(d[1], 'r');
+}
+
+TEST(Span, Iterator) {
+    const char text[] = "abc";
+    auto a = Span<const char>{text, sizeof(text) - 1};
+    const auto* it = a.begin();
+    ASSERT_NE(it, a.end());
+    ASSERT_EQ(*it, 'a');
+    ++it;
+    ASSERT_NE(it, a.end());
+    ASSERT_EQ(*it, 'b');
+    ++it;
+    ASSERT_NE(it, a.end());
+    ASSERT_EQ(*it, 'c');
+    ++it;
+    ASSERT_EQ(it, a.end());
+}
