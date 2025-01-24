edfe97a2ca48d983545b2e8a48383b3f4eb2aea0
Christopher Davis <84039785+chrsdavis@users.noreply.github.com>
2025-01-24 10:49:51 -0500

    feat: Optional (#19)
    * Update optional_test.cpp
    
    * Update optional.h
    
    * Update optional.h
    
    * Update optional.h
    
    * Update optional.h
    
    * Finished optional
    
    * weird compiler bug
    
    * Compiles now.
    
    * chore: add additional tests (nested, strView opt)
    
    ---------
    
    Co-authored-by: Madhav Shekhar Sharma <33770878+mdvsh@users.noreply.github.com>

--- core/include/core/optional.h
+++ core/include/core/optional.h
@@ -2,23 +2,422 @@
 #define LIB_OPTIONAL_H
 
 #include <utility>
+#include <type_traits>
+#include <stdexcept>
+#include <memory>
+#include <initializer_list>
+#include <algorithm>
+#include <concepts>
+#include <compare>
+
+
+struct nullopt_t {};
+inline static constexpr nullopt_t nullopt;
+
+class BadOptionalAccess final : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Bad optional access: Attempted to access value of an empty Optional";
+    }
+};
 
 template<typename T>
 class Optional {
+    // Helper type traits
+    template<class... Args>
+    static constexpr bool is_constructible_from_any_v = (std::is_constructible_v<T, Args> || ...);
+
+    template<class... Args>
+    static constexpr bool is_convertible_from_any_v = (std::is_convertible_v<Args, T> || ...);
+
+    template<class... Args>
+    static constexpr bool is_assignable_from_any_v = (std::is_assignable_v<Args, T> || ...);
+
+    template<typename U, typename V /*= U& or U&&*/>
+    static constexpr bool satisfies_conv_ctor = std::is_constructible_v<T, V> &&
+        !is_constructible_from_any_v<Optional<U>&, const Optional<U>&, Optional<U>&&, const Optional<U>&&> &&
+        !is_convertible_from_any_v<Optional<U>&, const Optional<U>&, Optional<U>&&, const Optional<U>&&>;
+
 public:
-    Optional() : has_value_(false), sentinel_(0) {}
-    Optional(const T& t) : has_value_(true), val_(t) {}
-    ~Optional() { if (has_value_) val_.~T(); }
+    constexpr Optional() noexcept : Optional(nullopt) {}
+    constexpr Optional(nullopt_t) noexcept : sentinel_{0}, has_value_{false} {}
+
+    constexpr Optional(const Optional& opt) requires std::is_trivially_copy_constructible_v<T> = default;
+    constexpr Optional(const Optional& opt) 
+        requires (std::is_copy_constructible_v<T> && !std::is_trivially_copy_constructible_v<T>)
+        : has_value_{opt.has_value_}
+    {
+        DirectInitFromOpt(opt);
+    }
+
+    constexpr Optional(Optional&& opt) requires std::is_trivially_move_constructible_v<T> = default;
+    constexpr Optional(Optional&& opt)
+        noexcept(std::is_nothrow_move_assignable_v<T>)
+        requires (std::is_move_constructible_v<T> && !std::is_trivially_move_constructible_v<T>)
+        : has_value_{opt.has_value_}
+    {
+        DirectInitFromOpt(std::forward(opt));
+    }
+
+    template<typename U>
+    constexpr explicit(!std::is_convertible_v<const U&, T>) Optional(const Optional<U>& opt)
+        requires satisfies_conv_ctor<U, const U&>
+        : has_value_{opt.has_value_}
+    {
+        DirectInitFromOpt(opt);
+    }
+
+    template<typename U>
+    constexpr explicit(!std::is_convertible_v<const U&, T>) Optional(Optional<U>&& opt)
+        requires satisfies_conv_ctor<U, U&&>
+        : has_value_{opt.has_value_}
+    {
+        DirectInitFromOpt(std::forward(opt));
+    }
+
+    // TODO: in place constructors
+
+    template <typename U = T>
+    constexpr explicit(!std::is_convertible_v<U&&, T>) Optional(U&& val)
+        requires (std::is_constructible_v<T, U&&> &&
+                  !std::is_same_v<std::remove_cvref_t<U>, Optional>)
+        : has_value_{true}
+    {
+        DirectInitVal(val);
+    }
+
+    constexpr ~Optional() requires std::is_trivially_destructible_v<T> = default;
+    constexpr ~Optional() { if (has_value_) val_.~T(); }
+
+    static constexpr bool is_trivially_copyable_v = std::is_trivially_copy_constructible_v<T> && 
+        std::is_trivially_copy_assignable_v<T> && std::is_trivially_destructible_v<T>;
+    static constexpr bool is_trivially_moveable_v = std::is_trivially_move_constructible_v<T> &&
+        std::is_trivially_move_assignable_v<T> && std::is_trivially_destructible_v<T>;
+
+    // Assignment
+    constexpr Optional& operator=(nullopt_t) noexcept { Reset(); }
+    constexpr Optional& operator=(const Optional& opt) requires is_trivially_copyable_v = default;
+    constexpr Optional& operator=(const Optional& opt)
+        requires (std::is_copy_constructible_v<T> && std::is_copy_assignable_v<T> && !is_trivially_copyable_v)
+    {
+        AssignFromOpt(opt);
+        return val_;
+    }
+    constexpr Optional& operator=(Optional&& opt)
+        noexcept(std::is_nothrow_move_assignable_v<T> && std::is_nothrow_move_constructible_v<T>)
+        requires is_trivially_moveable_v = default;
+    constexpr Optional& operator=(Optional&& opt)
+        noexcept(std::is_nothrow_move_assignable_v<T> && std::is_nothrow_move_constructible_v<T>)
+        requires (std::is_move_constructible_v<T> && std::is_move_assignable_v<T> && !is_trivially_moveable_v)
+    {
+        AssignFromOpt(std::forward(opt));
+        return val_;
+    }
+
+    template<typename U = T>
+    constexpr Optional& operator=(U&& value) requires
+        (std::is_constructible_v<T, U> && std::is_assignable_v<T&, U> &&
+        !std::is_same_v<std::remove_cvref_t<U>, Optional> &&
+        (!std::is_scalar_v<T> || !std::is_same_v<std::decay_t<U>, T>))
+    {
+        if (has_value_) { // val_ exists, so assign
+            val_ = std::forward(value);
+        } else { // must direct init it
+            DirectInitVal(std::forward(value));
+        }
+        return val_;
+    }
+
+    template<typename U, typename UU>
+    static constexpr bool satisfies_assign_reqs = satisfies_conv_ctor<U, UU> && std::is_assignable_v<T&, UU> &&
+        !is_assignable_from_any_v<Optional<U>&, const Optional<U>&, Optional<U>&&, const Optional<U>&&>;
+
+    template<typename U>
+    constexpr Optional& operator=(const Optional<U>& opt) requires satisfies_assign_reqs<U, const U&> {
+        AssignFromOpt(opt);
+        return val_;
+    }
+
+    template<typename U>
+    constexpr Optional& operator=(Optional<U>&& opt) requires satisfies_assign_reqs<U, U> {
+        AssignFromOpt(std::forward(opt));
+        return val_;
+    }
+
+    // Iterators (C++26)
+    // FIXME: add custom iterator type later
+    constexpr T* begin() noexcept { return has_value_ ? &val_ : nullptr; }
+    constexpr const T* begin() const noexcept { return has_value_ ? &val_ : nullptr; }
+    constexpr T* end() noexcept { return has_value_ ? &val_ + 1 : nullptr; }
+    constexpr const T* end() const noexcept { return has_value_ ? &val_ + 1 : nullptr; }
+
+    // Observers
+    constexpr const T* operator->() const noexcept { return &val_; }
+    constexpr T* operator->() noexcept { return &val_; }
+
+    constexpr const T& operator*() const& noexcept { return val_; }
+    constexpr T& operator*() & noexcept { return val_; }
+    constexpr const T&& operator*() const&& noexcept { return std::move(val_); }
+    constexpr T&& operator*() && noexcept { return std::move(val_); }
+
+    constexpr explicit operator bool() const noexcept { return has_value_; }
 
     [[nodiscard]]
-    bool HasValue() const { return has_value_; }
+    constexpr bool HasValue() const noexcept { return has_value_; }
 
-    
+    [[nodiscard]]
+    constexpr T& Value() & { return has_value_ ? val_ : throw BadOptionalAccess{}; }
+    [[nodiscard]]
+    constexpr const T& Value() const & { return has_value_ ? val_ : throw BadOptionalAccess{}; }
+    [[nodiscard]]
+    constexpr T&& value() && { return has_value_ ? std::move(val_) : throw BadOptionalAccess{}; }
+    [[nodiscard]]
+    constexpr const T&& value() const && { return has_value_ ? std::move(val_) : throw BadOptionalAccess{}; }
+
+    // TODO: add type constraints here https://en.cppreference.com/w/cpp/utility/optional/value_or
+    template<typename U >
+    constexpr T ValueOr(U&& default_value) const& { 
+        return has_value_ ? val_ : static_cast<T>(std::forward<U>(default_value));
+    }
+    template<typename U >
+    constexpr T ValueOr(U&& default_value) && {
+        return has_value_ ? std::move(val_) : static_cast<T>(std::forward<U>(default_value));
+    }
+
+    // Monadic Operations (C++23)
+    // TODO: maybe add these?
+
+    // Modifiers
+    constexpr void Swap(Optional& other)
+        noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_swappable_v<T>)
+        requires std::is_move_constructible_v<T>
+    {
+        if (this->has_value_ && other.has_value_) {
+            // TODO: use our own swap later
+            std::swap(this->val_, other.val_);
+        } else if (this->has_value_) {
+            other.DirectInitVal(std::move(this->val_));
+            this->ForceReset();
+        } else if (other.has_value_) {
+            this->DirectInitVal(std::move(other.val_));
+            other.ForceReset();
+        }
+    }
+
+    constexpr void Reset() noexcept {
+        if (has_value_) ForceReset();
+    }
+
+    // TODO: check Args can construct T?
+    template<class... Args>
+    constexpr T& Emplace(Args&&... args) {
+        Reset();
+        DirectInitVal(std::forward<Args>(args)...);
+        return val_;
+    }
+
+    // TODO: check Args can construct T?
+    template<typename U, class... Args>
+    constexpr T& Emplace(std::initializer_list<U> ilist, Args&&... args)
+        requires std::is_constructible_v<T, std::initializer_list<U>&, Args&&...>
+    {
+        Reset();
+        DirectInitVal(ilist, std::forward<Args>(args)...);
+        return val_;
+    }
 
 private:
-    bool has_value_;
-    union { char sentinel_; T val_; };
+    // assumes has_value_ is true
+    constexpr void ForceReset() {
+        if constexpr (!std::is_trivially_destructible_v<T>) {
+            val_.~T();
+        }
+        has_value_ = false;
+    }
 
+    // TODO: check T is constructible by Args?
+    template<class... Args>
+    constexpr void DirectInitVal(Args&&... args) {
+        std::construct_at(&val_, std::forward<Args>(args)...);
+        has_value_ = true;
+    }
+
+    // TODO: constrain Opt
+    template<typename Opt>
+    constexpr void DirectInitFromOpt(Opt&& opt) {
+        if (opt.has_value_) DirectInitVal(*std::forward(opt));
+        else sentinel_ = 0; // Required for constexpr
+    }
+
+    // TODO: constrain Opt
+    template<typename Opt>
+    constexpr void AssignFromOpt(Opt&& opt) {
+        if (this->has_value_ && opt.has_value_) {
+            val_ = *std::forward(opt);
+        } else if (this->has_value_) {
+            ForceReset();
+        } else if (opt.has_value_) {
+            DirectInitVal(std::forward(*opt));
+        }
+    }
+
+private:
+    union { char sentinel_; T val_; };
+    bool has_value_;
 };
+
+// Comparison Operators
+// compare with another optional
+template<class T, class U>
+constexpr bool operator==(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs == *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs == *rhs);
+    else return (lhs.HasValue() == rhs.HasValue());
+}
+
+template<class T, class U>
+constexpr bool operator!=(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs != *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs != *rhs);
+    else return (lhs.HasValue() != rhs.HasValue());
+}
+
+template<class T, class U>
+constexpr bool operator<(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs < *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs < *rhs);
+    else return !lhs.HasValue();
+}
+
+template<class T, class U>
+constexpr bool operator<=(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs <= *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs <= *rhs);
+    else return (lhs.HasValue() <= rhs.HasValue());
+}
+
+template<class T, class U>
+constexpr bool operator>(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs > *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs > *rhs);
+    else return lhs.HasValue();
+}
+
+template<class T, class U>
+constexpr bool operator>=(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires {{*lhs >= *rhs} -> std::convertible_to<bool>;}
+{
+    if (lhs && rhs) return (*lhs >= *rhs);
+    else return (lhs.HasValue() >= rhs.HasValue());
+}
+
+template<class T, class U>
+constexpr std::compare_three_way_result_t<T, U> operator<=>(const Optional<T>& lhs, const Optional<U>& rhs)
+    requires requires(T t, U u) {{t <=> u} -> std::convertible_to<std::compare_three_way_result_t<T, U>>;}
+{
+    return lhs && rhs ? *lhs <=> *rhs : lhs.HasValue() <=> rhs.HasValue();
+}
+
+// compare with nullopt
+template<class T>
+constexpr bool operator==(const Optional<T>& opt, nullopt_t) noexcept {
+    return !opt.HasValue();
+}
+
+template<class T>
+constexpr std::strong_ordering operator<=>(const Optional<T>& opt, nullopt_t) noexcept {
+    return (opt.HasValue() <=> false);
+}
+
+// compare with value
+template<class T, class U>
+constexpr bool operator==(const Optional<T>& opt, const U& value)
+    requires requires {{*opt == value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt == value : false;
+}
+template<class T, class U>
+constexpr bool operator==(const U& value, const Optional<T>& opt)
+    requires requires {{value == *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value == *opt : false;
+}
+
+template<class T, class U>
+constexpr bool operator!=(const Optional<T>& opt, const U& value)
+    requires requires {{*opt != value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt != value : false;
+}
+template<class T, class U>
+constexpr bool operator!=(const U& value, const Optional<T>& opt)
+    requires requires {{value != *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value != *opt : false;
+}
+
+template<class T, class U>
+constexpr bool operator<(const Optional<T>& opt, const U& value)
+    requires requires {{*opt < value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt < value : false;
+}
+template<class T, class U>
+constexpr bool operator<(const U& value, const Optional<T>& opt)
+    requires requires {{value < *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value < *opt : false;
+}
+
+template<class T, class U>
+constexpr bool operator<=(const Optional<T>& opt, const U& value)
+    requires requires {{*opt <= value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt <= value : false;
+}
+template<class T, class U>
+constexpr bool operator<=(const U& value, const Optional<T>& opt)
+    requires requires {{value <= *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value <= *opt : false;
+}
+
+template<class T, class U>
+constexpr bool operator>(const Optional<T>& opt, const U& value)
+    requires requires {{*opt > value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt > value : false;
+}
+template<class T, class U>
+constexpr bool operator>(const U& value, const Optional<T>& opt)
+    requires requires {{value > *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value > *opt : false;
+}
+
+template<class T, class U>
+constexpr bool operator>=(const Optional<T>& opt, const U& value)
+    requires requires {{*opt >= value} -> std::convertible_to<bool>;}
+{
+    return opt ? *opt >= value : false;
+}
+template<class T, class U>
+constexpr bool operator>=(const U& value, const Optional<T>& opt)
+    requires requires {{value >= *opt} -> std::convertible_to<bool>;}
+{
+    return opt ? value >= *opt : false;
+}
+
+template<class T, class U>
+constexpr std::compare_three_way_result_t<T, U> operator<=>( const Optional<T>& opt, const U& value )
+    requires requires(T t, U u) {{t <=> u} -> std::convertible_to<std::compare_three_way_result_t<T, U>>;}
+{
+    return opt ? *opt <=> value : std::strong_ordering::less;
+}
 
 #endif
--- core/tests/optional_test.cpp
+++ core/tests/optional_test.cpp
@@ -2,8 +2,162 @@
 
 #include <type_traits>
 #include <gtest/gtest.h>
+#include <vector>
+#include <utility>
 
+
+TEST(OptionalTests, Emplace) {
+    constexpr int i = 1;
+    Optional<int> o1;
+    EXPECT_EQ(o1, nullopt);
+    EXPECT_FALSE(o1);
+    o1.Emplace(i);
+    ASSERT_TRUE(o1);
+    EXPECT_EQ(o1, 1);
+
+    o1.Emplace(o1.Value());
+    ASSERT_TRUE(o1);
+    EXPECT_EQ(o1, i);
+}
+
+TEST(OptionalTests, NestedOptionals) {
+    Optional<Optional<int>> nested{1};
+    EXPECT_TRUE(nested->HasValue());
+}
+
+TEST(OptionalTests, StringViewOptimization) {
+    Optional<std::string_view> sv{"test"};
+    EXPECT_EQ(sv->length(), 4);
+}
+
+TEST(OptionalTests, EmplaceInitList) {
+    Optional<std::vector<int>> o1;
+    EXPECT_FALSE(o1);
+    o1.Emplace({1, 2, 3, 4});
+    ASSERT_TRUE(o1.HasValue());
+    EXPECT_EQ(o1->size(), 4);
+    const std::vector<int> v{1,2,3,4};
+    EXPECT_EQ(o1.Value(), v);
+}
+
+TEST(OptionalTests, Swap) {
+    Optional<int> o1{1};
+    Optional<int> o2{2};
+    o1.Swap(o2);
+    ASSERT_TRUE(o1 && o2);
+    EXPECT_EQ(o2, 1);
+    EXPECT_EQ(o1, 2);
+}
+
+TEST(OptionalTests, SwapNullopt) {
+    Optional<int> o1{1};
+    Optional<int> o2{};
+    o1.Swap(o2);
+    ASSERT_TRUE(o2);
+    EXPECT_FALSE(o1);
+    EXPECT_EQ(o2, 1);
+
+    Optional<int> o3{}, o4{}, o5{5};
+    o3.Swap(o4);
+    EXPECT_FALSE(o3 || o4);
+
+    o3.Swap(o5);
+    EXPECT_FALSE(o5);
+    ASSERT_TRUE(o3);
+    EXPECT_EQ(o3, 5);
+}
+
+TEST(OptionalTests, BadOptionalAccessError) {
+    Optional<int> opt;
+    EXPECT_THROW({int i = opt.Value();}, BadOptionalAccess);
+    EXPECT_THROW({const auto& i = opt.Value();}, BadOptionalAccess);
+    EXPECT_THROW(++opt.Value(), BadOptionalAccess);
+}
+
+TEST(OptionalTests, Comparisons) {
+    constexpr int i = 1, j = 2;
+    Optional<int> empty{};
+    EXPECT_TRUE(empty == nullopt);
+    EXPECT_TRUE(empty >= nullopt);
+    EXPECT_TRUE(empty <= nullopt);
+
+    Optional<int> o1{i};
+    EXPECT_TRUE(o1 != nullopt);
+    EXPECT_FALSE(o1 == nullopt);
+    EXPECT_TRUE(o1 > nullopt);
+
+    EXPECT_TRUE(o1 > empty);
+    EXPECT_TRUE(o1 >= empty);
+    EXPECT_TRUE(empty != o1);
+
+    EXPECT_TRUE(o1 == i);
+    EXPECT_TRUE(i == o1);
+    EXPECT_TRUE(o1 < j);
+    EXPECT_TRUE(j > o1);
+    EXPECT_TRUE(o1 <= j);
+    EXPECT_TRUE(j >= o1);
+    EXPECT_EQ(o1 <=> i, std::strong_ordering::equal);
+    EXPECT_EQ(o1 <=> j, std::strong_ordering::less);
+
+    Optional<int> o2{j};
+    EXPECT_TRUE(o1 < o2);
+    EXPECT_TRUE(o1 <= o2);
+    EXPECT_TRUE(o1 != o2);
+    EXPECT_EQ(o1 <=> o2, std::strong_ordering::less);
+
+    Optional<int> o3{i};
+    EXPECT_TRUE(o1 == o3);
+    EXPECT_EQ(o1 <=> o3, std::strong_ordering::equal);
+}
 
 TEST(OptionalTests, constexpr_stuff) {
-    // ...
+    constexpr Optional<int> empty1{};
+    constexpr Optional<int> empty2(nullopt);
+    constexpr Optional<int> empty3 = {};
+    constexpr Optional<int> empty4 = {nullopt};
+    constexpr Optional<int> empty5 = nullopt;
+
+    static_assert(!empty1);
+    static_assert(!empty2);
+    static_assert(!empty3);
+    static_assert(!empty4);
+    static_assert(!empty5);
+    static_assert(empty1.ValueOr(3) == 3);
+
+    constexpr Optional<int> val1{1};
+    constexpr Optional<int> val2(1);
+    constexpr Optional<int> val3 = 1;
+    constexpr Optional<int> val4 = {1};
+
+    static_assert(val1 == 1);
+    static_assert(val2 == 1);
+    static_assert(val3 == 1);
+    static_assert(val4 == 1);
+
+    static_assert(val1.Value() == 1);
+
+    constexpr int num = 2;
+    constexpr Optional<int> mov1{std::move(num)};
+    constexpr Optional<int> mov2(std::move(num));
+    constexpr Optional<int> mov3 = {std::move(num)};
+    constexpr Optional<int> mov4 = std::move(num);
+
+    static_assert(mov1 == 2);
+    static_assert(mov2 == 2);
+    static_assert(mov3 == 2);
+    static_assert(mov4 == 2); 
+
+    static_assert(val1 < mov1);
+    static_assert(val1 <= mov1);
+    static_assert(mov1 > val1);
+    static_assert(mov1 >= val2);
+    static_assert(val1 != mov1);
+    static_assert((val1 <=> mov1) == std::strong_ordering::less);
+    static_assert((val1 <=> 1) == std::strong_ordering::equal);
+}
+
+template<typename T> constexpr bool is_trivial = std::is_trivially_destructible<T>::value;
+TEST(OptionalTests, TypeTraits) {
+    static_assert(is_trivial<int>);
+    static_assert(is_trivial<Optional<int>>);
 }
