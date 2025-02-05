679eae1b822c5cd0b7f998d50b2b128808a56ffe
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-04 19:48:20 -0500

    Update algorithm_test.cpp

--- tests/algorithm_test.cpp
+++ tests/algorithm_test.cpp
@@ -3,10 +3,14 @@
 #include <type_traits>
 #include <gtest/gtest.h>
 #include <vector>
+#include <functional>
+#include <array>
 
 using std::vector;
 
-TEST(AlgorithmTests, ForEach_Basic) {
+constexpr void foo(int& s, int i) { s+= i; }
+
+TEST(AlgorithmTests, ForEach) {
     const vector<int> v = {1,2,3};
 
     int sum = 0;
@@ -20,4 +24,14 @@ TEST(AlgorithmTests, ForEach_Basic) {
     sum = 0;
     core::for_each(v.begin(), v.begin() + 1, [&](auto i){ sum += i;});
     EXPECT_EQ(sum, 1);
+}
+
+TEST(AlgorithmTests, ForEach_constexr) {
+   constexpr int sum = []() constexpr {
+        constexpr std::array<int, 3> v{1, 2, 3};  // constexpr-compatible array
+        int s = 0;
+        core::for_each(v.begin(), v.end(), [&s](int i) constexpr { s += i; });
+        return s;
+    }();
+    static_assert(sum == 6);
 }
