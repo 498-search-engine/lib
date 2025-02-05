dc16dd347376cfe93e14ee9c386f7af83e209dd1
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-04 19:14:40 -0500

    tessting set up

--- tests/algorithm_test.cpp
+++ tests/algorithm_test.cpp
@@ -2,3 +2,14 @@
 
 #include <type_traits>
 #include <gtest/gtest.h>
+#include <vector>
+
+using std::vector;
+
+TEST(AlgorithmTests, ForEach_Basic) {
+    vector<int> v = {1,2,3};
+    int sum = 0;
+    core::for_each(v.begin(), v.end(), [&](auto i){ sum += i;});
+    EXPECT_EQ(sum, 6);
+}
+
