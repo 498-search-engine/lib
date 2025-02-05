c0660b9f5dffd03aeea2e8efec401acdbe3ae314
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-04 19:18:54 -0500

    finished setting up test

--- tests/algorithm_test.cpp
+++ tests/algorithm_test.cpp
@@ -7,9 +7,17 @@
 using std::vector;
 
 TEST(AlgorithmTests, ForEach_Basic) {
-    vector<int> v = {1,2,3};
+    const vector<int> v = {1,2,3};
+
     int sum = 0;
     core::for_each(v.begin(), v.end(), [&](auto i){ sum += i;});
     EXPECT_EQ(sum, 6);
-}
 
+    sum = 0;
+    core::for_each(v.begin(), v.begin(), [&](auto i){ sum += i;});
+    EXPECT_EQ(sum, 0);
+
+    sum = 0;
+    core::for_each(v.begin(), v.begin() + 1, [&](auto i){ sum += i;});
+    EXPECT_EQ(sum, 1);
+}
