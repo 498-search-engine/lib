e8c8d52918e73ee7e66ad57db6979f8d24928aa7
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-10 10:44:03 -0500

    Fill

--- core/include/core/algorithm.h
+++ core/include/core/algorithm.h
@@ -371,6 +371,12 @@ constexpr BidirIt2 move_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
     return d_last;
 }
 
+template<class ForwardIt, class T>
+constexpr void fill(ForwardIt first, ForwardIt last, const T& value) {
+    for (; first != last; ++first)
+        *first = value;
+}
+
 }  // core
 
 #endif
