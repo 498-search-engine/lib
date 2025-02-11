4e88f1cb686ca4933be44aaf7700951429a83832
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-10 20:19:23 -0500

    generate

--- include/core/algorithm.h
+++ include/core/algorithm.h
@@ -402,6 +402,12 @@ constexpr OutputIt transform(InputIt1 first1, InputIt1 last1, InputIt2 first2,
     return d_first;
 }
 
+template<class ForwardIt, class Generator>
+constexpr void generate(ForwardIt first, ForwardIt last, Generator g) {
+    for (; first != last; ++first)
+        *first = g();
+}
+
 }  // core
 
 #endif
