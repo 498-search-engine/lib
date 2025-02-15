32b052a0ec2b2a3086ec0e622eebec66f487728d
iyasin <iyasin@umich.edu>
2025-02-15 01:22:49 -0500

    Tests

--- include/core/serializer.h
+++ include/core/serializer.h
@@ -2,22 +2,30 @@
 #define LIB_SERIALIZER_H
 
 
-#include <cstddef> // for size_t
+#include <cstddef>  // for size_t
 
-namespace core { 
 
-    template <typename T>
-    class AbstractSerializer {
-        public: 
+namespace core {
 
-        virtual ~AbstractSerializer() = default; 
-        // create a buffer and serialize object to it however you want
-        virtual char* serialize(const T& obj) const = 0; 
-        
-        // deserialize from some T that was AbstractSerializer::serialize into that buffer
-        virtual T deserialize(char* buffer, size_t size) const = 0;
-    };
+size_t roundUp(size_t length, size_t boundary) {
+    static const size_t oneless = boundary - 1, mask = ~(oneless);
+    return (length + oneless) & mask;
+}
+
+template<typename T>
+class AbstractSerializer {
+public:
+    virtual ~AbstractSerializer() = default;
+    // create a buffer and serialize object to it however you want
+    virtual char* serialize(const T& obj) const = 0;
+
+    // deserialize from some T that was AbstractSerializer::serialize into that buffer
+    virtual T deserialize(char* buffer, size_t size) const = 0;
+
+    // how many bytes we need to serialize object T
+    virtual size_t bytesRequired() const = 0;
 };
+};  // namespace core
 
 
 #endif
