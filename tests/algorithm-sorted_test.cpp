bab6b01e126f11b3438b9f124d3a6e8afb1eadb0
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-02-20 14:46:21 -0500

    upper bound

--- tests/algorithm-sorted_test.cpp
+++ tests/algorithm-sorted_test.cpp
@@ -59,3 +59,47 @@ TEST(AlgorithmTests, LowerBound_constexpr) {
     static_assert(core::lower_bound(a.begin(), a.end(), 1) == a.begin());
     static_assert(core::lower_bound(a.begin(), a.end(), 1, std::less{}) == a.begin());
 }
+
+TEST(AlgorithmTests, UpperBound) {
+    const vector<int> v1{1,2,3,4,5};
+    for (int i = 1; i <= 5; i++) {
+        auto it = core::upper_bound(v1.begin(), v1.end(), i);
+        EXPECT_EQ(it, v1.begin()+i);
+    }
+    auto it = core::upper_bound(v1.begin(), v1.end(), 0);
+    EXPECT_EQ(it, v1.begin());
+    it = core::upper_bound(v1.begin(), v1.end(), 6);
+    EXPECT_EQ(it, v1.end());
+
+    const vector<int> v2{5,4,3,2,1};
+    for (int i = 1; i <= 5; i++) {
+        auto it2 = core::upper_bound(v2.begin(), v2.end(), i, std::greater{});
+        std::cout << "i: " << i << " idx: " << (it2 - v2.begin()) << "\n";
+        EXPECT_EQ(it2, v2.begin()+(5-i+1));
+    }
+    auto it2 = core::upper_bound(v2.begin(), v2.end(), 6, std::greater{});
+    EXPECT_EQ(it2, v2.begin());
+    it2 = core::upper_bound(v2.begin(), v2.end(), 0, std::greater{});
+    EXPECT_EQ(it2, v2.end());
+
+    // Non random access
+    const list<int> l1{1,2,3,4,5};
+    for (int i = 1; i <= 5; i++) {
+        auto it3 = core::upper_bound(l1.begin(), l1.end(), i, std::less{});
+        EXPECT_EQ(it3, std::next(l1.begin(), i));
+    }
+    auto it3 = core::upper_bound(l1.begin(), l1.end(), 0);
+    EXPECT_EQ(it3, l1.begin());
+    it3 = core::upper_bound(l1.begin(), l1.end(), 6);
+    EXPECT_EQ(it3, l1.end());
+
+    // empty
+    vector<int> v3{};
+    EXPECT_EQ(core::upper_bound(v3.begin(), v3.end(), 0), v3.end());
+}
+
+TEST(AlgorithmTests, UpperBound_constexpr) {
+    constexpr array<int,4> a{1,2,3,4};
+    static_assert(core::upper_bound(a.begin(), a.end(), 1) == a.begin()+1);
+    static_assert(core::upper_bound(a.begin(), a.end(), 1, std::less{}) == a.begin()+1);
+}
