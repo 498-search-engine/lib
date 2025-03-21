84c2e2389b445b768f50016c13ee6e148a138e92
Anubhav Agarwal <anubhava@umich.edu>
2025-03-21 00:27:41 -0400

    Csr

---
+++ include/core/csr.h
@@ -0,0 +1,48 @@
+// authors: @Anubhav652
+
+#include <vector>
+
+namespace core {
+
+/*
+    This is a space-efficient approach to storing a sparse graph.
+    Primarily used for page rank.    
+*/
+class CSRMatrix {
+    public:
+        CSRMatrix(int nodes) : N_(nodes) { 
+            row_ptr_.resize(N_ + 1, 0); 
+        }
+
+        // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+        void AddEdge(int from, int to, double weight) {
+            col_idx_.push_back(to);
+            values_.push_back(weight);
+            row_ptr_[from + 1]++;
+        }
+
+        void Finalize() {
+            for (int i = 1; i <= N_; ++i) {
+                row_ptr_[i] += row_ptr_[i - 1];
+            }
+        }
+
+        std::vector<double> Multiply(const std::vector<double>& vec) {
+            std::vector<double> result(N_, 0.0);
+            #pragma omp parallel for
+            for (int i = 0; i < N_; ++i) {
+                for (int j = row_ptr_[i]; j < row_ptr_[i + 1]; ++j) {
+                    result[i] += values_[j] * vec[col_idx_[j]];
+                }
+            }
+            return result;
+        }
+
+    private:
+        std::vector<int> row_ptr_;
+        std::vector<int> col_idx_;
+        std::vector<double> values_;
+        int N_;
+};
+
+} // namespace core
