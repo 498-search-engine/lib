68636b0b44bd8c5267f747e78cc9d4d4c15d934a
ATShah03 <sarnav@umich.edu>
2025-04-09 02:14:38 -0400

    initial

---
+++ include/core/queue.h
@@ -0,0 +1,17 @@
+#ifndef LIB_QUEUE_H
+#define LIB_QUEUE_H
+
+#include <cstddef>
+#include <utility>
+#include "vector.h"
+
+namespace core {
+
+template<typename T, size_t N>
+struct Queue {
+    
+};
+
+}  // namespace core
+
+#endif
