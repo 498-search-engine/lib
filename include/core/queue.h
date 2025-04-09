1f052965282b927656547c0d5df23f145ceb294b
ATShah03 <sarnav@umich.edu>
2025-04-09 02:26:59 -0400

    queue done

--- include/core/queue.h
+++ include/core/queue.h
@@ -7,9 +7,36 @@
 
 namespace core {
 
-template<typename T, size_t N>
+template<typename T>
 struct Queue {
-    
+    //ring buffer queue
+    public:
+    void pop() {
+        if (back == front) return; //need some sort of logging w this
+        front += (front+1)%arr.size();
+        --sz;
+    }
+    void push(const T& element) {
+        auto new_b = (back +1) % arr.size();
+        (not arr.size()) or (new_b == front) ? arr.push_back(element) : arr[new_b] = elem;
+        back = (back +1) % arr.size();
+        ++sz;
+    }
+    T& front() {
+        return arr[front];
+    }
+    T& back(){
+        return arr[back];
+    }
+    bool empty() {
+        return front == back;
+    }
+    size_t size(){
+        return sz;
+    }
+    private:
+    vector<T> arr;
+    size_t front = 0, back = 0, sz = 0;
 };
 
 }  // namespace core
