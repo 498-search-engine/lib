df8429740696adbae8f6e9dd21108e7abd5fa73a
Anubhav652 <11070568+Anubhav652@users.noreply.github.com>
2025-04-22 21:56:27 -0400

    feat: add mlock (#71)

--- include/core/mem_map_file.h
+++ include/core/mem_map_file.h
@@ -4,16 +4,16 @@
  * @brief RAII wrappe for memory mapped file
  * @version 0.1
  * @date 2025-04-16
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 
 #ifndef LIB_MEM_MAP_FILE_H
 #define LIB_MEM_MAP_FILE_H
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
 namespace core {
 
@@ -21,16 +21,15 @@ class FileOpenFailure final : public std::exception {
 public:
     FileOpenFailure(const std::string& file, std::string msg = "")
         : what_("File open failure: failed to memory map file " + file + " " + msg) {}
-    const char* what() const noexcept override {
-        return what_.c_str();
-    }
+    const char* what() const noexcept override { return what_.c_str(); }
+
 private:
     std::string what_;
 };
 
 class MemMapFile {
 public:
-    MemMapFile(const std::string& path);
+    MemMapFile(const std::string& path, bool forceInMemory = false);
 
     MemMapFile(const MemMapFile&) = delete;
     MemMapFile& operator=(const MemMapFile&) = delete;
