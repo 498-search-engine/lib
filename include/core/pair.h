129ed10d70fa3b70861f86408e020ecabee856e2
ATShah03 <sarnav@umich.edu>
2025-04-27 18:57:36 -0400

    move constructor

--- include/core/pair.h
+++ include/core/pair.h
@@ -10,21 +10,21 @@ namespace core {
 
         constexpr Pair() = default;
 
-        // Copy constructor
-        constexpr Pair(const First& f, const Second& s) : first(f), second(s) {}
-    
-        // Move constructor
-        constexpr Pair(First&& f, Second&& s) noexcept(std::is_nothrow_move_constructible_v<First> && std::is_nothrow_move_constructible_v<Second>)
-            : first(std::move(f)), second(std::move(s)) {}
-    
-        // Constructor from std::pair
-        template <typename T1, typename T2>
-        constexpr Pair(const std::pair<T1, T2>& p) : first(p.first), second(p.second) {}
-    
-        // Move from std::pair
-        template <typename T1, typename T2>
-        constexpr Pair(std::pair<T1, T2>&& p) : first(std::move(p.first)), second(std::move(p.second)) {}
-    
+    // Move constructor (only allow moves)
+    constexpr Pair(First&& f, Second&& s) noexcept(
+        std::is_nothrow_move_constructible_v<First> && std::is_nothrow_move_constructible_v<Second>)
+        : first(std::move(f)), second(std::move(s)) {}
+
+    // Constructor from std::pair (copy)
+    template <typename T1, typename T2>
+    constexpr Pair(const std::pair<T1, T2>& p)
+        : first(p.first), second(p.second) {}
+
+    // Constructor from std::pair (move)
+    template <typename T1, typename T2>
+    constexpr Pair(std::pair<T1, T2>&& p)
+        : first(std::move(p.first)), second(std::move(p.second)) {}
+
         // Comparison operators
         constexpr bool operator==(const Pair& other) const {
             return first == other.first && second == other.second;
