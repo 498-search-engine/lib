cfde1faba7fccd4b4e7200f63f5037bd550863da
ATShah03 <sarnav@umich.edu>
2025-04-27 18:55:18 -0400

    comparison operators

--- include/core/pair.h
+++ include/core/pair.h
@@ -8,43 +8,47 @@ namespace core {
         First first;
         Second second;
 
-        // Default constructor
-    constexpr Pair() = default;
+        constexpr Pair() = default;
 
-    // Two-argument constructor
-    constexpr Pair(const First& f, const Second& s) : first(f), second(s) {}
-
-    // Move constructor
-    constexpr Pair(First&& f, Second&& s) : first(std::move(f)), second(std::move(s)) {}
-
-    // Allow implicit conversion from std::pair
-    template <typename T1, typename T2>
-    constexpr Pair(const std::pair<T1, T2>& p) : first(p.first), second(p.second) {}
-
-    // Comparison operators
-    constexpr bool operator==(const Pair& other) const {
-        return first == other.first && second == other.second;
-    }
-
-    constexpr bool operator!=(const Pair& other) const {
-        return !(*this == other);
-    }
-
-    constexpr bool operator<(const Pair& other) const {
-        return (first < other.first) || (!(other.first < first) && second < other.second);
-    }
-
-    constexpr bool operator>(const Pair& other) const {
-        return other < *this;
-    }
-
-    constexpr bool operator<=(const Pair& other) const {
-        return !(other < *this);
-    }
-
-    constexpr bool operator>=(const Pair& other) const {
-        return !(*this < other);
-    }
+        // Copy constructor
+        constexpr Pair(const First& f, const Second& s) : first(f), second(s) {}
+    
+        // Move constructor
+        constexpr Pair(First&& f, Second&& s) noexcept(std::is_nothrow_move_constructible_v<First> && std::is_nothrow_move_constructible_v<Second>)
+            : first(std::move(f)), second(std::move(s)) {}
+    
+        // Constructor from std::pair
+        template <typename T1, typename T2>
+        constexpr Pair(const std::pair<T1, T2>& p) : first(p.first), second(p.second) {}
+    
+        // Move from std::pair
+        template <typename T1, typename T2>
+        constexpr Pair(std::pair<T1, T2>&& p) : first(std::move(p.first)), second(std::move(p.second)) {}
+    
+        // Comparison operators
+        constexpr bool operator==(const Pair& other) const {
+            return first == other.first && second == other.second;
+        }
+    
+        constexpr bool operator!=(const Pair& other) const {
+            return !(*this == other);
+        }
+    
+        constexpr bool operator<(const Pair& other) const {
+            return (first < other.first) || (!(other.first < first) && second < other.second);
+        }
+    
+        constexpr bool operator>(const Pair& other) const {
+            return other < *this;
+        }
+    
+        constexpr bool operator<=(const Pair& other) const {
+            return !(other < *this);
+        }
+    
+        constexpr bool operator>=(const Pair& other) const {
+            return !(*this < other);
+        }
     };
 
 }
