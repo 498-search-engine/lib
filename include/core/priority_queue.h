59ef1fed309cf22f61bf3836339d6544f19596fd
Arnav Shah <sarnav@umich.edu>
2025-04-27 07:10:39 -0500

    pq impl + tests (#79)

--- include/core/priority_queue.h
+++ include/core/priority_queue.h
@@ -8,39 +8,55 @@
 
 namespace core {
 
-template<typename T, typename Container = vector<T>, typename COMP = std::less<T>>
+template<typename T, typename Container = Vector<T>, typename COMP = std::less<T>>
 struct PriorityQueue {
     //binary heap impl of priority queue
     public:
     PriorityQueue() = default;
     PriorityQueue(const std::initializer_list<T>& il){
         for (auto i : il){
-            arr.push_back(i);
+            arr.pushBack(i);
         }
+        sz = arr.size();
         heapify();
     }
+
+    void printPQ(){
+        for (auto i : arr){
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+
     void pop() {
-        if (back == front) return; //need some sort of logging w this
-        front += (front+1)%arr.size();
+        if (empty()) return;
+        std::swap(arr[0], arr[sz-1]);
         --sz;
+        fixDown(0);
     }
 
     void push(const T& element) {
-        auto new_b = (back +1) % arr.size();
-        (not arr.size()) or (new_b == front) ? arr.push_back(element) : arr[new_b] = elem;
-        back = (back +1) % arr.size();
+        if (sz == arr.size()) {
+            arr.pushBack(element);
+        } else {
+            arr[sz] = element;
+        }
+        fixUp(sz);
         ++sz;
     }
 
     void push(T&& element) {
-        auto new_b = (back +1) % arr.size();
-        (not arr.size()) or (new_b == front) ? arr.push_back(std::move(elem)) : arr[new_b] = std::move(elem);
-        back = (back +1) % arr.size();
+        if (sz == arr.size()) {
+            arr.pushBack(std::move(element));
+        } else {
+            arr[sz] = std::move(element);
+        }
+        fixUp(sz);
         ++sz;
     }
 
     const T& top() {
-        return arr[front];
+        return arr[0];
     }
     
     bool empty() {
@@ -52,17 +68,48 @@ struct PriorityQueue {
     private:
     Container arr;
     COMP compare;
-    uint32_t sz;
+    uint32_t sz = 0;
 
-    uint32_t getIndex(const uint32 i) { return i-1; }
+    uint32_t getIndex(const uint32_t i) { return i-1; }
 
-    void fixUp(uint32 idx) {
-        while (idx > 0)
+    void fixUp(uint32_t idx) {
+        while (idx > 0) {
+            uint32_t parent = getIndex(idx) / 2;
+            if (!compare(arr[parent], arr[idx])) {
+                break;
+            }
+            std::swap(arr[parent], arr[idx]);
+            idx = parent;
+        }
+    }
+
+    void fixDown(uint32_t idx){
+        while (true) {
+            uint32_t left = 2 * idx + 1;
+            uint32_t right = 2 * idx + 2;
+            uint32_t largest = idx;
+    
+            if (left < sz && compare(arr[largest], arr[left])) {
+                largest = left;
+            }
+            if (right < sz && compare(arr[largest], arr[right])) {
+                largest = right;
+            }
+    
+            if (largest == idx) {
+                break;
+            }
+            
+            std::swap(arr[idx], arr[largest]);
+            idx = largest;
+        }
     }
 
 
     void heapify(){
-
+        for (int i = sz-1; i >= 0; --i) {
+            fixDown(i);
+        }
     }
 };
 
