028f65d0ba32b40a7c14ebc743d6e20df21a3e4a
ATShah03 <sarnav@umich.edu>
2025-04-27 18:51:50 -0400

    comparison operators

--- include/core/pair.h
+++ include/core/pair.h
@@ -8,13 +8,43 @@ namespace core {
         T1 first;
         T2 second;
 
-        constexpr bool operator==(const Pair& other) const {
-            return first == other.first && second == other.second;
-        }
+        // Default constructor
+    constexpr Pair() = default;
 
-        constexpr bool operator<(const Pair& other) const {
-            return (first < other.first) || (!(other.first < first) && second < other.second);
-        }
+    // Two-argument constructor
+    constexpr Pair(const First& f, const Second& s) : first(f), second(s) {}
+
+    // Move constructor
+    constexpr Pair(First&& f, Second&& s) : first(std::move(f)), second(std::move(s)) {}
+
+    // Allow implicit conversion from std::pair
+    template <typename T1, typename T2>
+    constexpr Pair(const std::pair<T1, T2>& p) : first(p.first), second(p.second) {}
+
+    // Comparison operators
+    constexpr bool operator==(const Pair& other) const {
+        return first == other.first && second == other.second;
+    }
+
+    constexpr bool operator!=(const Pair& other) const {
+        return !(*this == other);
+    }
+
+    constexpr bool operator<(const Pair& other) const {
+        return (first < other.first) || (!(other.first < first) && second < other.second);
+    }
+
+    constexpr bool operator>(const Pair& other) const {
+        return other < *this;
+    }
+
+    constexpr bool operator<=(const Pair& other) const {
+        return !(other < *this);
+    }
+
+    constexpr bool operator>=(const Pair& other) const {
+        return !(*this < other);
+    }
     };
 
 }
