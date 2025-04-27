8bc420d0c1f368372bb829708cf50a079eddb5da
ATShah03 <sarnav@umich.edu>
2025-04-27 18:59:49 -0400

    move constructor

--- include/core/pair.h
+++ include/core/pair.h
@@ -1,56 +1,67 @@
 #ifndef LIB_PAIR_H
 #define LIB_PAIR_H
-
+#include <type_traits>
+#include <utility> // for std::move and std::forward
 
 namespace core {
-    template<typename First, typename Second>
-    struct Pair{
-        First first;
-        Second second;
 
-        constexpr Pair() = default;
+template <typename First, typename Second>
+struct Pair {
+    First first;
+    Second second;
 
-    // Move constructor (only allow moves)
-    constexpr Pair(First&& f, Second&& s) noexcept(
-        std::is_nothrow_move_constructible_v<First> && std::is_nothrow_move_constructible_v<Second>)
+    // Default constructor
+    constexpr Pair() = default;
+
+    // Move constructor
+    constexpr Pair(First&& f, Second&& s)
         : first(std::move(f)), second(std::move(s)) {}
 
-    // Constructor from std::pair (copy)
-    template <typename T1, typename T2>
-    constexpr Pair(const std::pair<T1, T2>& p)
-        : first(p.first), second(p.second) {}
+    // Copy constructor
+    template <typename F = First, typename S = Second,
+              typename = std::enable_if_t<std::is_copy_constructible_v<F> && std::is_copy_constructible_v<S>>>
+    constexpr Pair(const F& f, const S& s)
+        : first(f), second(s) {}
 
-    // Constructor from std::pair (move)
-    template <typename T1, typename T2>
-    constexpr Pair(std::pair<T1, T2>&& p)
-        : first(std::move(p.first)), second(std::move(p.second)) {}
+    // Perfect forwarding constructor
+    template <typename F, typename S,
+              typename = std::enable_if_t<!std::is_same_v<Pair, std::decay_t<F>>>>
+    constexpr Pair(F&& f, S&& s)
+        : first(std::forward<F>(f)), second(std::forward<S>(s)) {}
 
-        // Comparison operators
-        constexpr bool operator==(const Pair& other) const {
-            return first == other.first && second == other.second;
-        }
-    
-        constexpr bool operator!=(const Pair& other) const {
-            return !(*this == other);
-        }
-    
-        constexpr bool operator<(const Pair& other) const {
-            return (first < other.first) || (!(other.first < first) && second < other.second);
-        }
-    
-        constexpr bool operator>(const Pair& other) const {
-            return other < *this;
-        }
-    
-        constexpr bool operator<=(const Pair& other) const {
-            return !(other < *this);
-        }
-    
-        constexpr bool operator>=(const Pair& other) const {
-            return !(*this < other);
-        }
-    };
+    // Copy constructor and move constructor defaulted if available
+    constexpr Pair(const Pair&) = default;
+    constexpr Pair(Pair&&) noexcept = default;
 
-}
+    constexpr Pair& operator=(const Pair&) = default;
+    constexpr Pair& operator=(Pair&&) noexcept = default;
+
+    // Comparison operators
+    constexpr bool operator==(const Pair& other) const {
+        return first == other.first && second == other.second;
+    }
+
+    constexpr bool operator!=(const Pair& other) const {
+        return !(*this == other);
+    }
+
+    constexpr bool operator<(const Pair& other) const {
+        return (first < other.first) || (!(other.first < first) && second < other.second);
+    }
+
+    constexpr bool operator<=(const Pair& other) const {
+        return !(other < *this);
+    }
+
+    constexpr bool operator>(const Pair& other) const {
+        return other < *this;
+    }
+
+    constexpr bool operator>=(const Pair& other) const {
+        return !(*this < other);
+    }
+};
+
+} // namespace core
 
 #endif
